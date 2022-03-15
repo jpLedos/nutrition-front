7a7b4171c99d4dc7a44e399934e90521e7122807
Jean-Pierre Ledos <jpledos@gmail.com>
2022-03-15 19:57:07 +0100

    API_URL move to heroko api https

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 // export const API_URL = 'https://localhost:8000/api/'
 
-export const API_URL = 'http://nutrition-jp-api.herokuapp.com/api/'
+export const API_URL = 'https://nutrition-jp-api.herokuapp.com/api/'
